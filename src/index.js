4f9c7d0aa7cd62af7217ffe645b9687ec4c296c8
wzc <wzc>
2021-03-08 23:38:53 +0800

    react-redux优化—简写+Provider组件

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.jsx';
 import store from './redux/store';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App/>,document.getElementById('root'));
-
-// 监测redux中状态的改变，改变时重新渲染App组件
-store.subscribe(() => {
-  ReactDOM.render(<App/>,document.getElementById('root'));
-});
+ReactDOM.render(
+    <Provider store={store}>
+      <App/>
+    </Provider>,
+    document.getElementById('root')
+);
